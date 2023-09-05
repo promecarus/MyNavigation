8751295a3501868951a0e705123dbf98356824cb
promecarus <haikalslipi@gmail.com>
2023-09-05 21:29:06 +0700

    part04

--- app/src/main/java/com/promecarus/mynavigation/DetailCategoryFragment.kt
+++ app/src/main/java/com/promecarus/mynavigation/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.promecarus.mynavigation.databinding.FragmentDetailCategoryBinding
 
 class DetailCategoryFragment : Fragment() {
@@ -31,6 +32,10 @@ class DetailCategoryFragment : Fragment() {
 
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescription.text = "Stock : $dataDescription"
+
+        binding.btnProfile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
     }
 
     override fun onDestroy() {
